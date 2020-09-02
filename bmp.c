67e4911fc80f74a3aa11d37503403c944e82ca1c
MrOsteoporosis <sanderverheijen@outlook.com>
2020-09-02 15:42:49 +0200

    bitmap fixed but it doesnt work so its broken? kill me pls

--- bmp.c
+++ bmp.c
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/29 13:42:34 by averheij      #+#    #+#                 */
-/*   Updated: 2020/07/01 14:55:02 by averheij      ########   odam.nl         */
+/*   Updated: 2020/09/02 15:42:27 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ int		write_bitmaprgb(t_data *data, int fd)
 		x = 0;
 		while (x < data->resx)
 		{
-			if (write(fd, linestart + x, data->bits_per_pixel >> 3) == -1)
+			if (write(fd, linestart + (x * data->bits_per_pixel >> 3),
+						data->bits_per_pixel >> 3) == -1)
 				return (1);
 			if (write(fd, &zero, 1) == -1)
 				return (1);
@@ -42,13 +43,15 @@ int		write_bitmaprgb(t_data *data, int fd)
 
 int		write_bitmapinfoheader(t_data *data, int fd)
 {
-	static int	header[11] = {40, -1, -1, 0, -1, 0, 0, 0, 0, 0, 0};
+	static int	header[11] = {40, -1, -1, 1, -1, 0, 0, 0, 0, 0, 0};
 	static int	size[11] = {4, 4, 4, 2, 2, 4, 4, 4, 4, 4, 4};
 	int			i;
 
 	header[1] = data->resx;
 	header[2] = data->resy;
 	header[4] = data->bits_per_pixel + 8;
+	/*header[4] = data->bits_per_pixel;*/
+	header[9] = 1 << data->bits_per_pixel;
 	i = 0;
 	while (i < 11)
 	{
@@ -61,12 +64,13 @@ int		write_bitmapinfoheader(t_data *data, int fd)
 
 int		write_bitmapfileheader(t_data *data, int fd)
 {
-	static int	header[5] = {19778, -1, 0, 0, 54};
+	static int	header[5] = {0x4D42, -1, 0, 0, 54};
 	static int	size[5] = {2, 4, 2, 2, 4};
 	int			i;
 
 	header[1] = (data->resx * data->resy * ((data->bits_per_pixel >> 3) + 1))
-					+ 55;
+	/*header[1] = (data->resx * data->resy * ((data->bits_per_pixel >> 3)))*/
+					+ 54;
 	i = 0;
 	while (i < 5)
 	{
@@ -81,7 +85,7 @@ int		write_bmp(t_data *data)
 {
 	int		fd;
 
-	fd = open("./save.bmp", O_CREAT | O_WRONLY);
+	fd = open("./save.bmp", O_CREAT | O_WRONLY, 0666);
 	if (fd == -1)
 		return (1);
 	if (write_bitmapfileheader(data, fd))
